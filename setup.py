356537e1993bce7e3678032ec15f71006f59b874
akashagalave <akashagalaveaaa1@gmail.com>
2024-11-22 22:00:27 +0530

    Setup.py file ready

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """This function will return list of requirements"""
+    requirement_list:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            lines=file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement != '-e .':
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print('requirements.txt file not found')
+    
+    return requirement_list
+
+setup(
+    name="NETWORKSECURITY",
+    version="0.0.1",
+    author="akash agalave",
+    author_email="akashagalaveaaa1@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
